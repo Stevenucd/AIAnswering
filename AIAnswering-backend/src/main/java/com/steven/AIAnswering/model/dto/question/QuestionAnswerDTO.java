bcb74c93f28036d31d018da987bdde08c6225e2f
Stevenucd <157613651+Stevenucd@users.noreply.github.com>
2024-07-12 16:00:18 +0800

    frontend updated

---
+++ AIAnswering-backend/src/main/java/com/steven/AIAnswering/model/dto/question/QuestionAnswerDTO.java
@@ -0,0 +1,20 @@
+package com.steven.AIAnswering.model.dto.question;
+
+import lombok.Data;
+
+/**
+ * 题目答案封装类（用于 AI 评分）
+ */
+@Data
+public class QuestionAnswerDTO {
+
+    /**
+     * 题目
+     */
+    private String title;
+
+    /**
+     * 用户答案
+     */
+    private String userAnswer;
+}
